feb178a751b6eb94849005029ca716aa10b21e64
Luis Fellipy Bett <luisfbett@gmail.com>
2024-12-14 19:02:24 +0000

    change file paths and add packages and webpack config

---
+++ practical-challenges/html-css-js/member-club/src/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+//CSS
+import "./styles/global.css"
+import "./styles/header.css"
+import "./styles/main.css"
+import "./styles/user.css"
+import "./styles/card.css"
+import "./styles/history.css"
+import "./styles/progress.css"
+
+//JS
+import './modules/input.js'
