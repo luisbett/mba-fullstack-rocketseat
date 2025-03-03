a5fd94b0c30e1181b51de3efc631acce9717471f
Luis Fellipy Bett <luisfbett@gmail.com>
2025-03-03 20:38:45 +0000

    first commit

---
+++ practical-challenges/react/marketplace-web/src/App.tsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="zinc-900 text-2xl">Hello World</h1>
+    </div>
+  )
+}
