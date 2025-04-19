e96b27da156fd2430f8058bfff6a3c04a4f5f1a5
Luis Fellipy Bett <luisfbett@gmail.com>
2025-04-19 16:21:12 +0100

    add listener to app layout component to redirect user to sign in if the axios response status is unauthorized

--- practical-challenges/react/marketplace-web/src/pages/_layouts/app.tsx
+++ practical-challenges/react/marketplace-web/src/pages/_layouts/app.tsx
@@ -1,8 +1,32 @@
-import { Outlet } from 'react-router'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+
+          if (status === 401) {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="font-Poppins bg-background min-h-screen leading-[1.2] antialiased">
       <Header />
