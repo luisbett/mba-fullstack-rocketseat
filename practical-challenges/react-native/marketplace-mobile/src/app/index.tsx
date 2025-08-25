f507e7fd6cb338e7d999bc4cd8ba545a3ab160cb
Luis Fellipy Bett <luisfbett@gmail.com>
2025-08-25 21:07:57 +0100

    add fonts and gluestack ui lib

--- practical-challenges/react-native/marketplace-mobile/src/app/index.tsx
+++ practical-challenges/react-native/marketplace-mobile/src/app/index.tsx
@@ -1,7 +1,12 @@
 import { router } from "expo-router";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { useFonts, DMSans_700Bold } from '@expo-google-fonts/dm-sans'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { config } from '../../config/gluestack-ui.config'
 
 export default function Index() {
+    const [ fontsLoaded ] = useFonts({ DMSans_700Bold })
+
     function handleSignUp() {
         router.navigate('/sign-up')
     }
@@ -11,16 +16,22 @@ export default function Index() {
     }
     
     return (
-        <View style={styles.container}>
-            <Text>Acesse sua conta</Text>
-            <TouchableOpacity onPress={handleSignIn}>
-                <Text>Acessar</Text>
-            </TouchableOpacity>
+        <GluestackUIProvider config={config}>
+            { fontsLoaded ? (
+                <Center flex={1} gap={16}>
+                    <Text>Acesse sua conta</Text>
+                    <TouchableOpacity onPress={handleSignIn}>
+                        <Text>Acessar</Text>
+                    </TouchableOpacity>
 
-            <TouchableOpacity onPress={handleSignUp}>
-                <Text>Cadastrar</Text>
-            </TouchableOpacity>
-        </View>
+                    <TouchableOpacity onPress={handleSignUp}>
+                        <Text>Cadastrar</Text>
+                    </TouchableOpacity>
+                </Center>
+            ) : (
+                <View />
+            )}
+        </GluestackUIProvider>
     )
 }
 
