e916a21459e0d3538676a35dcaa83f3057d118c6
Luis Fellipy Bett <luisfbett@gmail.com>
2025-08-26 16:52:51 +0100

    add components to profile screen

--- practical-challenges/react-native/marketplace-mobile/src/app/(tabs)/profile.tsx
+++ practical-challenges/react-native/marketplace-mobile/src/app/(tabs)/profile.tsx
@@ -1,13 +1,80 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Box, Center, Heading, ScrollView, VStack } from "@gluestack-ui/themed";
+
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import { AccessIcon, CallIcon, ImageUpload01Icon, Logout01Icon, Mail02Icon, UserIcon } from "@hugeicons/core-free-icons";
+
+import { Button } from "@/components/button";
+import { Input } from "@/components/input";
 
 export default function Profile() {
+    function handleProfileUpdate() {}
+
+    function handleLogout() {}
+
     return (
-        <View style={styles.container}>
-            <Text>Perfil</Text>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <VStack flex={1} px={'$10'} mb='$5' position="relative">
+                <Center mt='$16' mb='$5'>
+                    <Box 
+                        bgColor="$shape"
+                        width='$30'
+                        height='$30'
+                        rounded='$xl'
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <HugeiconsIcon icon={ImageUpload01Icon} color='#F24D0D' width='32px' height='32px' />
+                    </Box>
+                </Center>
+
+                <Button
+                    icon={Logout01Icon}
+                    iconColor='#DC3545'
+                    variant="outline"
+                    position="absolute"
+                    top='$16'
+                    right='$6'
+                    onPress={handleLogout}
+                />
+
+                <Input 
+                    title='Nome'
+                    icon={UserIcon}
+                    placeholder="Seu nome completo"
+                />
+
+                <Input 
+                    title='Telefone'
+                    icon={CallIcon}
+                    placeholder="(00) 00000-0000"
+                />
+
+                <Heading fontSize='$md' mt='$5' mb='$5'>Acesso</Heading>
+
+                <Input 
+                    title='E-mail'
+                    icon={Mail02Icon}
+                    placeholder="mail@examplo.br"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+
+                <Input
+                    title='Senha atual'
+                    icon={AccessIcon}
+                    isPassword
+                    placeholder="Sua senha" 
+                />
+
+                <Input
+                    title='Nova senha'
+                    icon={AccessIcon}
+                    isPassword
+                    placeholder="Sua nova senha"
+                />
+                
+                <Button title="Atualizar cadastro" mt='$6' onPress={handleProfileUpdate} />
+            </VStack>
+        </ScrollView>
     )
 }
-
-const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', gap: 16 }
-})
--- practical-challenges/react-native/marketplace-mobile/src/components/button.tsx
+++ practical-challenges/react-native/marketplace-mobile/src/components/button.tsx
@@ -1,21 +1,23 @@
 import { Button as GluestackButton, Spinner, Text } from '@gluestack-ui/themed'
 import { ArrowRight02Icon } from '@hugeicons/core-free-icons'
-import { HugeiconsIcon } from '@hugeicons/react-native'
+import { HugeiconsIcon, IconSvgElement } from '@hugeicons/react-native'
 import { ComponentProps } from 'react'
 
 type Props = ComponentProps<typeof GluestackButton> & {
-    title: string
+    title?: string
+    icon?: IconSvgElement
+    iconColor?: string
     isLoading?: boolean
     withArrow?: boolean
 }
 
-export function Button({ title, isLoading, withArrow = false, variant = 'solid', ...rest }: Props) {
+export function Button({ title, icon, iconColor = '#F24D0D', isLoading = false, withArrow = false, variant = 'solid', ...rest }: Props) {
     return (
         <GluestackButton 
-            w='$full'
-            h='$14'
+            w={ icon ? '$10' : '$full' }
+            h={ icon ? '$10' : '$14' }
             bg={ variant === 'solid' ? '$orangebase' : '$transparent' }
-            borderColor='$orangebase'
+            borderColor={ icon ? iconColor : '$orangebase'}
             borderWidth={1}
             borderRadius='$2lg'
             justifyContent={ !withArrow || isLoading ? 'center' : 'space-between' }
@@ -24,8 +26,13 @@ export function Button({ title, isLoading, withArrow = false, variant = 'solid',
             { isLoading ?
                 <Spinner color={ variant === 'solid' ? '$white' : '$orangebase' }/> : 
                 <>
-                    <Text color={ variant === 'solid' ? '$white' : '$orangebase' }>{title}</Text>
-                    { withArrow && <HugeiconsIcon icon={ArrowRight02Icon} color={ variant === 'solid' ? '#FFFFFF' : '#F24D0D' } /> }
+                    { icon ?
+                        <HugeiconsIcon icon={icon} color={iconColor} /> : (
+                        <>
+                            <Text color={ variant === 'solid' ? '$white' : '$orangebase' }>{title}</Text>
+                            { withArrow && <HugeiconsIcon icon={ArrowRight02Icon} color={ variant === 'solid' ? '#FFFFFF' : '#F24D0D' } /> }
+                        </>
+                    )}
                 </>
             }
         </GluestackButton>
